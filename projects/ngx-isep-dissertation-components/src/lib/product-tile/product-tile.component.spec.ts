07d0f9535fea2d4a79551251ae230967679fec9e
Carina Alas <1181695@isep.ipp.pt>
2024-04-10 22:42:32 +0100

    Adding test and fixing decimalPipe

--- projects/ngx-isep-dissertation-components/src/lib/product-tile/product-tile.component.spec.ts
+++ projects/ngx-isep-dissertation-components/src/lib/product-tile/product-tile.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ProductTileComponent } from './product-tile.component';
+import { DecimalPipe } from '@angular/common';
 
 describe('ProductTileComponent', () => {
   let component: ProductTileComponent;
@@ -8,10 +8,13 @@ describe('ProductTileComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ProductTileComponent ]
+      declarations: [ ProductTileComponent ],
+      providers: [ DecimalPipe ] // Providing DecimalPipe for testing
     })
     .compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(ProductTileComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +22,29 @@ describe('ProductTileComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should format finalPriceToDisplay correctly', () => {
+    component.finalPrice = 20.99;
+    component.ngOnInit();
+    expect(component.finalPriceToDisplay).toBe('20.99');
+  });
+
+  it('should format priceWithoutPromoToDisplay correctly and set isRed to true', () => {
+    component.priceWithoutPromo = 25.99;
+    component.ngOnInit();
+    expect(component.priceWithoutPromoToDisplay).toBe('25.99');
+    expect(component.isRed).toBe(true);
+  });
+
+  it('should have default values for inputs when not provided', () => {
+    expect(component.imageSrc).toBe('https://via.placeholder.com/312x400');
+    expect(component.hasTag).toBe(false);
+    expect(component.tagColor).toBe('#1E2832');
+    expect(component.tagText).toBe('SALE');
+    expect(component.productCategory).toBe('');
+    expect(component.productName).toBe('');
+    expect(component.finalPrice).toBe(0);
+    expect(component.priceWithoutPromo).toBeUndefined();
   });
 });
--- projects/ngx-isep-dissertation-components/src/lib/product-tile/product-tile.component.ts
+++ projects/ngx-isep-dissertation-components/src/lib/product-tile/product-tile.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { DecimalPipe } from '@angular/common';
 
 @Component({
@@ -6,24 +6,27 @@ import { DecimalPipe } from '@angular/common';
   templateUrl: './product-tile.component.html',
   styleUrls: ['./product-tile.component.scss']
 })
-export class ProductTileComponent {
-  @Input() imageSrc: string ="https://via.placeholder.com/312x400";
+export class ProductTileComponent implements OnInit {
+  @Input() imageSrc: string = "https://via.placeholder.com/312x400";
   @Input() hasTag: boolean = false;
   @Input() tagColor: string = "#1E2832";
   @Input() tagText: string = "SALE";
   @Input() productCategory: string = "";
   @Input() productName: string = "";
   @Input() finalPrice: number = 0;
-  finalPriceToDisplay: string | null = '';
   @Input() priceWithoutPromo: number | undefined;
+  finalPriceToDisplay: string | null = '';
   priceWithoutPromoToDisplay: string | null = '';
   isRed: boolean = false;
 
   constructor(private decimalPipe: DecimalPipe) {
+  }
+
+  ngOnInit(): void {
     this.finalPriceToDisplay = this.decimalPipe.transform(this.finalPrice, '1.2-2');
-    if(this.priceWithoutPromo){
+
+    if (this.priceWithoutPromo) {
       this.priceWithoutPromoToDisplay = this.decimalPipe.transform(this.priceWithoutPromo, '1.2-2');
       this.isRed = true;
-    }
-  }
+    }  }
 }
