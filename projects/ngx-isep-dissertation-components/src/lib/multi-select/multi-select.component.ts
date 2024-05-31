ba299cdb7d244b25186982adbd0b564b7a5521d1
Carina Alas <1181695@isep.ipp.pt>
2024-05-31 15:32:08 +0100

    Multi-select - css

--- projects/ngx-isep-dissertation-components/src/lib/multi-select/multi-select.component.ts
+++ projects/ngx-isep-dissertation-components/src/lib/multi-select/multi-select.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'lib-multi-select',
+  selector: 'isep-lib-multi-select',
   templateUrl: './multi-select.component.html',
   styleUrls: ['./multi-select.component.scss']
 })
 export class MultiSelectComponent {
+  categories = [
+    { id: 1, name: 'Coats', selected: false },
+    { id: 2, name: 'Dresses', selected: false },
+    { id: 3, name: 'Pants', selected: false },
+    { id: 4, name: 'Shorts', selected: false },
+    { id: 5, name: 'Category With A Long Name', selected: false }
+  ];
+  dropdownOpen = false;
 
+  constructor() { }
+
+  ngOnInit(): void { }
+
+  toggleDropdown(): void {
+    this.dropdownOpen = !this.dropdownOpen;
+  }
+
+  toggleCategorySelection(category: any): void {
+    category.selected = !category.selected;
+  }
 }
--- projects/ngx-isep-dissertation-components/src/lib/multi-select/multi-select.module.ts
+++ projects/ngx-isep-dissertation-components/src/lib/multi-select/multi-select.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from "@angular/core";
 
 import { CommonModule } from "@angular/common";
 import { MultiSelectComponent } from "./multi-select.component";
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
     declarations: [MultiSelectComponent],
-    imports: [CommonModule],
+    imports: [CommonModule, FormsModule],
     exports: [MultiSelectComponent]
 })
 export class MultiSelectModule { }
